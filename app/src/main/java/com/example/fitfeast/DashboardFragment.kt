1bdf416d1bb48d144675469eca6d857e027879fc
AmgT94 <79568135+AmgT94@users.noreply.github.com>
2024-03-07 20:40:46 -0500

    Implemented Navigation Drawer Access and UI Fixes
    - Enabled access to the navigation drawer from the MedicationFragment by integrating the DrawerLayout with the fragment.
    - Adjusted the visibility of sign-in and sign-up tabs within the MedicationFragment to ensure a consistent user experience.
    - Resolved issues with the display of duplicate titles in the DashboardFragment by correctly setting up the Toolbar as the ActionBar and managing titles through the NavController.
    - Addressed UI inconsistencies and streamlined the application's navigation by modifying MainActivity and DashboardFragment to better handle the navigation drawer's state and action bar title updates.

--- app/src/main/java/com/example/fitfeast/DashboardFragment.kt
+++ app/src/main/java/com/example/fitfeast/DashboardFragment.kt
@@ -1,70 +1,69 @@
 package com.example.fitfeast
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.drawerlayout.widget.DrawerLayout
-import com.google.android.material.navigation.NavigationView
 import androidx.appcompat.widget.Toolbar
-import android.widget.ImageButton
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
 import com.example.fitfeast.databinding.FragmentDashboardBinding
 
 class DashboardFragment : Fragment() {
 
     private lateinit var binding: FragmentDashboardBinding
-    private lateinit var toggle: ActionBarDrawerToggle
+
+    interface DrawerController {
+        fun setDrawerLocked(locked: Boolean)
+    }
+
+    private var drawerController: DrawerController? = null
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is DrawerController) {
+            drawerController = context
+            drawerController?.setDrawerLocked(false)
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        // Inflate the layout for this fragment using binding
         binding = FragmentDashboardBinding.inflate(inflater, container, false)
-        val view = binding.root
 
+        // Set the toolbar as the app bar for the activity
         val toolbar: Toolbar = binding.toolbar
-        (activity as AppCompatActivity).setSupportActionBar(toolbar)
+        val appCompatActivity = activity as AppCompatActivity
+        appCompatActivity.setSupportActionBar(toolbar)
 
-        toggle = ActionBarDrawerToggle(
-            activity, binding.drawerLayout, toolbar,
-            R.string.navigation_drawer_open,
-            R.string.navigation_drawer_close
-        )
+        appCompatActivity.supportActionBar?.title = ""
 
-        binding.drawerLayout.addDrawerListener(toggle)
-        toggle.syncState()
-
-        setupNavigationView()
-
-        return view
+        return binding.root
     }
 
-    private fun setupNavigationView() {
-        binding.navView.setNavigationItemSelectedListener { menuItem ->
-            when (menuItem.itemId) {
-                R.id.nav_dashboard -> {
-                    findNavController().navigate(R.id.dashboardFragment)
-                }
-                R.id.nav_goals -> {
-                }
-                R.id.nav_weight -> {
-                }
-                R.id.nav_medication -> {
-                    findNavController().navigate(R.id.medicationFragment)
-                }
-            }
-            menuItem.isChecked = true
-            binding.drawerLayout.closeDrawers()
-            true
-        }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // Set the toolbar as the app bar.
+        (activity as? AppCompatActivity)?.setSupportActionBar(binding.toolbar)
+
+        //(activity as? AppCompatActivity)?.supportActionBar?.title = getString(R.string.dashboard)
     }
 
     override fun onResume() {
         super.onResume()
-        (activity as AppCompatActivity).supportActionBar?.title = getString(R.string.dashboard)
+        // Set the title in the ActionBar
+        //(activity as? AppCompatActivity)?.supportActionBar?.title = getString(R.string.dashboard)
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        // Lock the drawer when fragment detaches if necessary
+        drawerController?.setDrawerLocked(true)
+        drawerController = null
     }
 }
--- app/src/main/java/com/example/fitfeast/MainActivity.kt
+++ app/src/main/java/com/example/fitfeast/MainActivity.kt
@@ -2,16 +2,24 @@ package com.example.fitfeast
 
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.example.fitfeast.databinding.ActivityMainBinding
+import androidx.appcompat.widget.Toolbar
+import com.google.android.material.navigation.NavigationView
+import androidx.appcompat.app.ActionBarDrawerToggle
 import com.google.android.material.tabs.TabLayout
+import androidx.core.view.GravityCompat
+import androidx.lifecycle.MutableLiveData
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
+    private lateinit var drawerToggle: ActionBarDrawerToggle
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,40 +27,115 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Set up the toolbar
+        val toolbar: Toolbar = findViewById(R.id.toolbar_main)
+        setSupportActionBar(toolbar)
+
         // Set up the NavHostFragment and NavController
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
 
         setupTabLayout()
 
-        // Listen for navigation changes to show or hide the TabLayout accordingly
+        setupDrawer()
+
+        // Listen for navigation changes to update UI components accordingly
         navController.addOnDestinationChangedListener { _, destination, _ ->
             when (destination.id) {
-                R.id.userProfileCreationFragment, R.id.dashboardFragment -> {
-                    binding.tabLayout.visibility = View.GONE
+                R.id.dashboardFragment -> {
+                    setDrawerEnabled(true)
+                    setTabsVisibility(false)
                     supportActionBar?.show()
                     supportActionBar?.title = getString(R.string.dashboard)
                 }
+                R.id.userProfileCreationFragment -> {
+                    setDrawerEnabled(false)
+                    setTabsVisibility(false)
+                    supportActionBar?.hide()
+                }
                 R.id.profileFragment -> {
-                    binding.tabLayout.visibility = View.GONE
+                    setDrawerEnabled(true)
+                    setTabsVisibility(false)
                     supportActionBar?.show()
                     supportActionBar?.title = getString(R.string.profile)
                 }
+                R.id.medicationFragment -> {
+                    setDrawerEnabled(true)
+                    setTabsVisibility(false)
+                    supportActionBar?.show()
+                    supportActionBar?.title = getString(R.string.medication)
+                }
+                R.id.signInFragment, R.id.signUpFragment -> {
+                    setDrawerEnabled(false)
+                    setTabsVisibility(true)
+                    supportActionBar?.hide()
+                }
                 else -> {
-                    binding.tabLayout.visibility = View.VISIBLE
+                    setDrawerEnabled(false)
+                    setTabsVisibility(false)
+                    supportActionBar?.show()
                     supportActionBar?.title = getString(R.string.app_name)
                 }
             }
         }
+
+
+
+    }
+
+    private fun setupDrawer() {
+        drawerToggle = ActionBarDrawerToggle(
+            this,
+            binding.drawerLayout,
+            binding.toolbarMain,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
+        )
+
+        binding.drawerLayout.addDrawerListener(drawerToggle)
+        drawerToggle.syncState()
+
+        // Set up the NavigationView listener
+        binding.navView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.nav_dashboard -> navController.navigate(R.id.dashboardFragment)
+                R.id.nav_goals -> {
+                }
+                R.id.nav_weight -> {
+                }
+                R.id.nav_medication -> navController.navigate(R.id.medicationFragment)
+                // ... add other menu items here
+            }
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
+            true
+        }
+
+        // Find the header view and the edit button within it
+        val headerView = binding.navView.getHeaderView(0)
+        val editProfileButton = headerView.findViewById<ImageButton>(R.id.editProfileButton)
+
+        // Set up the click listener for the edit button
+        editProfileButton.setOnClickListener {
+            navController.navigate(R.id.profileFragment)
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
+        }
+    }
+
+
+    private fun setDrawerEnabled(enabled: Boolean) {
+        val lockMode = if (enabled) DrawerLayout.LOCK_MODE_UNLOCKED else DrawerLayout.LOCK_MODE_LOCKED_CLOSED
+        binding.drawerLayout.setDrawerLockMode(lockMode)
+        drawerToggle.isDrawerIndicatorEnabled = enabled
+    }
+
+    private fun setTabsVisibility(visible: Boolean) {
+        binding.tabLayout.visibility = if (visible) View.VISIBLE else View.GONE
     }
 
     private fun setupTabLayout() {
-        val tabTitles = arrayOf("Sign In", "Sign Up")
-
         binding.tabLayout.apply {
-            removeAllTabs()
-            addTab(newTab().setText(tabTitles[0]))
-            addTab(newTab().setText(tabTitles[1]))
+            addTab(newTab().setText("Sign In"))
+            addTab(newTab().setText("Sign Up"))
 
             addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
                 override fun onTabSelected(tab: TabLayout.Tab) {
@@ -67,4 +150,3 @@ class MainActivity : AppCompatActivity() {
         }
     }
 }
-
